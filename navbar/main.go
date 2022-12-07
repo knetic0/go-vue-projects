ae26a3330be6f2005649089a8df3d140de133d74
Mehmet Solak <72500821+knetic0@users.noreply.github.com>
2022-12-07 23:12:09 +0300

    Update main.go

--- navbar/main.go
+++ navbar/main.go
@@ -48,9 +48,6 @@ func init() {
 	connString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 	db, err = sql.Open("postgres", connString)
 	CheckError(err)
-	defer db.Close()
-	err = db.Ping()
-	CheckError(err)
 	GetDatas()
 }
 
