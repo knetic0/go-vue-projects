e99c994b57ea6e16fc39fbfb80e4c8195b824a9a
Mehmet Solak <72500821+knetic0@users.noreply.github.com>
2022-12-07 21:29:53 +0300

    Add files via upload

--- second-app-2/second-app/src/App.vue
+++ second-app-2/second-app/src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="app" class="container">
-    <input type="text" ref="input" v-model="header" />
-    <button v-on:click="headerYazdir" @click="listAppend">Yazdir</button>
-    <h1> {{ header }}</h1>
-    <ul>
-      <li v-for="(data, key) in itemList" :key="key">
-        {{ data }} 
-        <a href="#" v-if="data[0] === 'M'">{{ data }}</a>
-      </li>
-    </ul>
+    <center>
+      <br />
+      <input class="form-control w-50" placeholder="Enter a todo" type="text" ref="input" v-model="header" />
+      <br />
+      <button class="btn btn-primary" v-on:click="headerYazdir" @click="listAppend">Yazdir</button> 
+      <h1> {{ header }}</h1>
+      <br />
+      <ul class="list-group w-25 list-group-flush">
+        <li class="list-group-item" v-for="(data, key) in itemList" :key="key">
+          {{ data }} 
+          <button class="btn btn-primary" @click="isDone(key)">✓</button>
+          <a href="#" v-if="data[0] === 'M'">{{ data }}</a>
+        </li>
+      </ul>
+    </center>
   </div>
 </template>
 <script>
@@ -25,7 +31,8 @@
       this.socket = new WebSocket("ws://localhost:9100/socket")
       this.socket.onmessage = (event) => { 
         this.message = event.data
-        this.fromDatabase(this.message)     
+        if (this.message != "")
+          this.fromDatabase(this.message)     
       }
     },
     created() {
@@ -38,11 +45,14 @@
       listAppend() {
         let msg = { "title" : this.search, "done" : false}
         this.socket.send(JSON.stringify(msg))
-        this.itemList.push(this.$refs.input.value)
+        this.itemList.push(msg)
         this.header = ""
       },
       fromDatabase(data) {
         this.itemList.push(data)
+      },
+      isDone(index) {
+        this.itemList.splice(index, 1)
       }
     }
   }
