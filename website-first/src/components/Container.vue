2bf54c7bb515aa91906107b0cec104cf28b10e25
Mehmet Solak <72500821+knetic0@users.noreply.github.com>
2022-12-09 20:17:51 +0300

    Add files via upload

---
+++ website-first/src/components/Container.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="p-5 my-2 bg-dark text-white">
+        <h1 class="text-center mb-5"> Anasayfa </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, officiis in. Eveniet laborum error vero accusantium fuga illum incidunt rem provident laudantium sed reprehenderit corrupti cum expedita sint, veniam ab labore perferendis totam vitae earum libero soluta inventore nulla est. Minima, odit magni tempora nam temporibus magnam qui, a at est quaerat deserunt ullam sunt commodi neque exercitationem placeat, iusto dolor. Odit eius impedit sunt, reprehenderit itaque soluta quasi non porro amet, voluptas cum dolorum enim ullam quibusdam! Eligendi omnis ut, officiis ratione accusamus sapiente in ipsam commodi quas cupiditate consectetur blanditiis nemo modi nobis molestiae recusandae magnam? Alias autem eaque delectus perferendis incidunt ipsum corrupti, ullam, recusandae ratione totam explicabo laudantium. Cupiditate corporis quasi recusandae sequi illo voluptatum commodi deserunt corrupti distinctio autem, unde praesentium harum error cum mollitia officiis asperiores culpa placeat nisi! A quo animi ullam aspernatur incidunt ipsa nobis delectus rerum. Impedit expedita a voluptatem placeat doloribus necessitatibus praesentium, iure dolor qui cumque quam nesciunt id iste nobis possimus deserunt rem, beatae eum fugit in accusantium eius. Maxime, deserunt esse laboriosam neque culpa minus nobis distinctio eveniet quam nesciunt error temporibus vero nostrum eum voluptatem, quasi dolor alias est. Quos necessitatibus dolorem dolor? Laudantium facilis fugiat eveniet dolorem, officiis quidem obcaecati, deserunt aliquam dolores aperiam repellat molestias voluptates corrupti laborum optio eum asperiores esse et assumenda. Dolores doloribus ipsam, labore reprehenderit velit totam illo, placeat magnam debitis odit, molestiae at dicta in quidem accusamus eius doloremque? Eveniet porro dolorem voluptatibus incidunt corrupti officiis ut. Rerum adipisci saepe maxime nihil possimus totam, est ratione molestias dolores porro impedit obcaecati eaque delectus pariatur assumenda reiciendis error, explicabo accusantium officiis libero itaque at ducimus laborum velit. Facere quae ducimus, explicabo ratione perferendis sit vel? Totam ipsum quis a possimus mollitia dolorem, est corporis! Quae obcaecati error vero ea ex quod est nemo dolor laudantium cum nulla, nihil quia molestias harum expedita! Temporibus tempore, cum veritatis necessitatibus qui inventore sunt et non quam perspiciatis eaque cupiditate rem est, nostrum enim velit accusamus? Corporis saepe quo officia quis iusto soluta labore aperiam minima, harum unde, recusandae excepturi illum delectus dolores doloribus quod ratione. Iste, sequi esse aliquam perferendis dolores odit deserunt excepturi eos assumenda nesciunt fuga laboriosam amet perspiciatis eum quisquam totam voluptatem pariatur adipisci iusto mollitia cupiditate corrupti optio nulla itaque. Incidunt odio recusandae voluptatum optio quidem distinctio voluptas impedit, cumque illum perferendis qui cupiditate laboriosam, sit magnam inventore. Harum.</p>
+    </div>
+    <div class="p-5 my-2 bg-white text-black">
+        <h1 class="text-center mb-5">Blog</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium odit sed reprehenderit quis voluptates optio illum harum sapiente quisquam, amet quas inventore, laudantium assumenda corrupti itaque odio quia veritatis voluptatum quibusdam? Perferendis ipsa ad officia illum quidem optio enim voluptatibus eum. Dolorum nihil expedita atque velit, voluptate officia voluptatibus sit error voluptatum libero. Numquam minus natus ad modi ratione, possimus eligendi enim a ab reiciendis excepturi provident nostrum ipsum veritatis veniam commodi incidunt voluptatibus. Facere aperiam, eum blanditiis dignissimos id animi error perspiciatis fugit vitae numquam, tempore minus recusandae explicabo laborum! Voluptate, sit et alias quae ipsa, cum non, sint illum quas a doloremque. Vitae aliquam commodi et possimus officiis eveniet a, accusamus eum cumque voluptatibus voluptatum veniam voluptas adipisci fugit alias dolorum, doloribus at! Veritatis est qui vel deleniti laudantium cum ducimus odit ut cupiditate itaque. Aspernatur officia ducimus totam nisi laborum. Delectus perferendis placeat facilis illum repellendus itaque molestiae! Nemo, explicabo! Minima obcaecati maxime natus deserunt eos quis, inventore rerum, eveniet rem, ipsum ducimus dignissimos cupiditate laboriosam. Quo fugit praesentium in, quod quisquam veritatis recusandae. Eaque accusamus mollitia aspernatur ratione. Esse, atque vel inventore nam eveniet libero deleniti, labore iure repellat adipisci sunt maxime commodi quibusdam ab ullam perspiciatis iusto quo velit. Expedita quasi quae explicabo cumque veritatis odio suscipit exercitationem recusandae dignissimos at inventore deleniti dolores, iusto accusantium distinctio mollitia iste autem eius laudantium cum eveniet repellendus ab! Corporis sed tempore minus voluptatum. Labore voluptate, non quae molestias, odio praesentium minima quaerat reprehenderit error dolore sit.</p>
+    </div>
+    <div class="p-5 my-2 bg-dark text-white">
+        <h1 class="text-center mb-5">Hakkımızda</h1>
+        <div class="d-flex">
+            <img class="w-25 rounded-circle" src="../assets/versebytelogo.jpg" alt="Versebyte Logo">
+            <p class="ms-5 mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil illum impedit distinctio voluptate odio quisquam repellendus a, cum veniam qui mollitia perspiciatis. Consequatur maiores cupiditate nulla asperiores quidem deserunt id necessitatibus culpa eaque sit! Nostrum, tempore possimus laboriosam nesciunt provident mollitia molestias, deserunt unde dicta eaque temporibus. Consectetur cum soluta minima eveniet odit veniam, voluptate rem nisi nihil voluptatum numquam cumque architecto minus maiores. Fuga et dicta corrupti, vero velit fugit voluptatibus ea soluta culpa, adipisci voluptates. Exercitationem recusandae accusamus animi sed, praesentium non quam. Corrupti repellendus iusto nesciunt tempore magnam eaque consectetur dolorum officia officiis, eos libero alias ab similique non natus veritatis, delectus nostrum ipsam? Ex corrupti in beatae consequatur voluptatibus quaerat provident maiores sapiente temporibus nam explicabo, laborum ea neque minus quo tempore, nulla reprehenderit animi molestias incidunt porro? Quo neque repudiandae illum hic, doloribus numquam optio, distinctio nesciunt qui debitis nulla mollitia magnam et illo nihil.</p>
+        </div>
+    </div>
+    <div class="p-5 my-2 bg-white text-black">
+        <h1 class="text-center mb-5">İletişim</h1>
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="basic-addon1">@</span>
+            </div>
+            <input type="text" class="form-control" placeholder="Adınız" aria-label="Username" aria-describedby="basic-addon1">
+        </div>
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Mail Adresiniz" aria-label="Recipient's username" aria-describedby="basic-addon2">
+        <div class="input-group-append">
+            <span ref="mailarea" class="input-group-text" id="basic-addon2">@example.com</span>
+        </div>
+        <div class="input-group mt-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text w-100 h-100">Düşünceleriniz...</span>
+            </div>
+            <textarea ref="textarea" class="form-control" aria-label="With textarea"></textarea>
+        </div>
+        <div class="col mt-3 text-center">
+            <button @click="TakeMessage()" type="button" class="btn btn-dark justify-content-center">Gönder</button>
+        </div>
+</div>
+    </div>
+</template>
+<script>
+    export default {
+        Name : "Container",
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
---
+++ website-first/src/components/NavigationBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-white bg-white">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"> <span class="fs-2 ms-4">{{ Navbar }}</span></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav me-auto mt-1 mb-2 mb-lg-0 ms-5">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="Anasayfa">Anasayfa</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="Blog">Blog</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="Hakkımızda">Hakkımızda</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="İletişim">İletişim</a>
+          </li>
+        </ul>
+        <span class="navbar-text me-5 mt-1">
+          Gerçekçi Bir Vizyon ile...
+        </span>
+      </div>
+    </div>
+  </nav>
+  </template>
+  <script>
+    export default {
+      name : "NavigationBar",
+      data() { return {
+        Navbar : "Versebyte",
+        NavbarText : "Gerçekçi Bir Vizyon İle...",
+      }}
+    }
+  </script>
+  <style scoped>
+  </style>
