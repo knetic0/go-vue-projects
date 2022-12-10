b5393a06a72a7a72eb2c589496279db3b5fad200
Mehmet Solak <72500821+knetic0@users.noreply.github.com>
2022-12-10 19:14:07 +0300

    Add files via upload

--- website-first/src/App.vue
+++ website-first/src/App.vue
@@ -16,30 +16,11 @@
     },
     data() {
       return {
-        socket : "",
-        message : "",
-        mail : "",
+      
       }
     },
-
-    mounted() {
-      this.socket = new WebSocket("ws://localhost:9100/")
-    },
-
     data() { return {
       background_img : "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fgetwallpapers.com%2Fwallpaper%2Ffull%2Fc%2F5%2Fd%2F688482-large-software-wallpapers-1920x1080-hd-1080p.jpg&f=1&nofb=1&ipt=cb321fada2ded5225ca308e5dc18ba7ddf2501aba2ce81cd25cc58b8ea012aa6&ipo=images",
     }},
-
-    methods : {
-      TakeMessage() {
-        this.message = this.$refs.textarea.value
-        this.mail = this.$refs.mailarea.value
-      },
-
-      SendInfo() {
-        let msg = {"message": null, "mail": null}
-        this.socket.send(JSON.stringify(msg))
-      }
-    }
   }
 </script>
